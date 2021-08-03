7b37c92990366b0ef4a295f8d387aee10aa0a8d1
jalofernandez <jalofernandez@gmail.com>
2021-08-03 17:49:29 +0200

    feat: clients data table redesigned

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,124 +22,122 @@
       />
     </div>
 
-    <div class="overflow-x-auto">
-      <div class="min-w-screen flex items-center justify-center font-sans overflow-hidden">
-        <div class="w-full">
-          <div class="bg-white dark:text-white shadow-md rounded my-6">
-            <table class="min-w-max w-full table-auto">
-              <thead>
-                <tr class="bg-gray-200 text-gray-500 dark:bg-navy-700 dark:text-navy-300 uppercase text-sm leading-normal">
-                  <th class="py-3 px-2 text-center">Acciones</th>
-                  <th class="py-3 px-2 text-left">ID</th>
-                  <th class="py-3 px-2 text-left">Raza</th>
-                  <th class="py-3 px-2 text-left">Perrete</th>
-                  <th class="py-3 px-2 text-center">Caracter</th>
-                  <th class="py-3 px-2 text-left">Dueño</th> 
-                  <!-- <th class="py-3 px-2 text-left">Notas</th> -->
-                  <!-- <th class="py-3 px-2 text-right">Citas</th> -->
-                </tr>
-              </thead>
-              <tbody class="text-gray-600 text-base font-light">
-                <tr
-                  v-for="(client, index) in filteredCustomers"
-                  :key="index"
-                  class="border-b border-gray-200 dark:border-navy-700 
-                  bg-white 
-                  dark:bg-navy-900 hover:bg-gray-100 dark:hover:bg-navy-700"
+    <div class='overflow-x-auto w-full'>
+      <!-- <table class='w-full whitespace-nowrap bg-white divide-y divide-gray-300 dark:divide-navy-700 overflow-hidden'> -->
+      <table
+        v-if="!loader"
+        class='w-full bg-white divide-y divide-gray-300 dark:divide-navy-700 overflow-hidden'
+      >
+        <thead class="bg-gray-200 dark:bg-navy-700">
+          <tr class=" text-gray-500 dark:text-navy-300 uppercase text-sm leading-normal">
+            <th class="px-3 py-2 text-left">ID</th>
+            <th class="px-3 py-2 text-left">Perrete</th>
+            <th class="px-3 py-2 text-center">Caracter</th>
+            <th class="px-3 py-2 text-left">Dueño</th>
+            <th class="px-3 py-2 text-left">Citas</th>
+            <th class="w-60 px-3 py-2 text-left">Notas</th>
+            <th class="w-24 px-3 py-2 text-right">Acciones</th>
+          </tr>
+        </thead>
+        <tbody class="divide-y divide-gray-200 dark:divide-navy-700 text-gray-600 text-base font-light">
+          <tr
+            v-for="(client, index) in filteredCustomers"
+            :key="index"
+            class="border-b border-gray-200 dark:border-navy-700 bg-white dark:bg-navy-900 hover:bg-gray-50 dark:hover:bg-navy-700"
+          >
+            <!-- Client ID -->
+            <td class="px-3 py-2">
+              <p class="text-green-200 dark:text-green-700 text-sm font-semibold tracking-wide">
+                {{ index + 1 }}
+              </p>
+            </td>
+
+            <!-- Doggie Name + Breed -->
+            <td class="px-3 py-2">
+              <div class="flex items-center space-x-3">
+                <div class="w-10 h-10 hidden lg:inline-flex churruskus">
+                  <img
+                    src="../assets/doggies/bichon-maltes.png"
+                    class="w-10 h-10 object-cover rounded-full border-gray-200 border transform hover:scale-125"
+                    title="Bichón maltés avatar por defecto"
+                    alt="Bichón maltés avatar por defecto"
+                  />
+                </div>
+                <div>
+                  <p class="dark:text-sky-300 font-semibold tracking-wide">
+                    {{ client.name }}
+                  </p>
+                  <p class="text-sm text-gray-400 dark:text-sky-300">
+                    {{ client.breed }}
+                  </p>
+                </div>
+              </div>
+            </td>
+
+            <!-- Doggie Mood -->
+            <td class="px-3 py-2 text-center">
+              <span
+                v-if="client.mood"
+                class="py-0.5 px-2 rounded-full text-xs"
+                :class="`bg-${getColorMood(client.mood)}-200 text-${getColorMood(client.mood)}-700`"
+              >
+                {{ client.mood }}
+              </span>
+            </td>
+
+            <!-- Owner Name + Phone -->
+            <td class="px-3 py-2">
+              <p v-if="client.owner" class="dark:text-sky-300 capitalize">
+                {{ client.owner }}
+              </p>
+              <p v-if="client.ownerPhone" class="text-gray-500 dark:text-sky-300 text-xs font-semibold tracking-wide">
+                {{ client.ownerPhone }}
+              </p>
+              <p v-if="client.guardian" class="mt-1 dark:text-sky-300">
+                {{ client.guardian }}
+              </p>
+              <p v-if="client.guardianPhone" class="text-gray-500 dark:text-sky-300 text-xs font-semibold tracking-wide">
+                {{ client.guardianPhone }}
+              </p>
+            </td>
+
+            <!-- Number of Dates -->
+            <td class="px-3 py-2 text-center">
+              <span class="text-blue-300 dark:text-blue-700 font-medium">
+                4
+              </span>
+            </td>
+
+            <!-- Comments -->
+            <td class="w-60 px-3 py-2 text-left">
+              <p class="text-xs text-navy-700 dark:text-sky-300 leading-4 md:leading-5">
+                {{ client.comment }}
+              </p>
+            </td>
+
+            <!-- Actions -->
+            <td class="w-24 px-3 py-2 text-right">
+              <p>
+                <router-link
+                  :to="{ name: 'Editor', params: { id: client.id } }"
+                  class="w-20 block border-0 rounded py-1 px-3 bg-blue-500 hover:bg-blue-600 focus:outline-none text-white hover:underline text-xs text-center tracking-wide"
                 >
-                  <!-- Actions -->
-                  <td class="py-3 px-2 text-center">
-                    <div class="flex item-center justify-center">
-                      <router-link
-                        :to="{ name: 'Editor', params: { id: client.id } }"
-                        class="text-white bg-blue-500 border-0 py-1 px-3 focus:outline-none hover:bg-blue-600 rounded text-xs tracking-wide mr-2"
-                      >
-                        Editar
-                      </router-link>
-                      <button @click="removeClient(client.id)" class="text-white bg-red-500 border-0 py-1 px-3 focus:outline-none hover:bg-red-600 rounded text-xs tracking-wide h-6">
-                        Eliminar
-                      </button>
-                    </div>
-                  </td>
-                  <!-- Client ID -->
-                  <td class="py-3 px-2 text-left whitespace-nowrap">
-                    <div class="flex items-center">
-                      <span class="font-xs text-green-200">
-                        {{ index + 1 }}
-                      </span>
-                    </div>
-                  </td>
-                  <!-- Doggie Breed -->
-                  <td class="py-3 px-2 text-center">
-                    <div class="flex items-center">
-                      <div class="flex items-center justify-center mr-2 churruskus">
-                        <img
-                          src="../assets/doggies/bichon-maltes.png"
-                          class="w-6 h-6 rounded-full border-gray-200 border transform hover:scale-125"
-                        />
-                      </div>
-                      <small class="mr-1 text-navy-700 dark:text-sky-300">
-                        {{ client.breed }}
-                      </small>
-                    </div>
-                  </td>
-                  <!-- Doggie Name -->
-                  <td class="py-3 px-2 text-left">
-                    <div class="flex items-center">
-                      <span class="font-medium capitalize text-navy-700 dark:text-sky-300">
-                        {{ client.name }}
-                      </span>
-                    </div>
-                  </td>
-                  <!-- Doggie Mood -->
-                  <td class="py-3 px-2 text-center">
-                    <span
-                      v-if="client.mood"
-                      class="py-0.5 px-2 rounded-full text-xs"
-                      :class="`bg-${getColorMood(
-                        client.mood
-                      )}-200 text-${getColorMood(client.mood)}-700`"
-                    >
-                      {{ client.mood }}
-                    </span>
-                  </td>
-                  <!-- Owner Name + Phone -->
-                  <td class="py-3 px-2 text-left whitespace-nowrap">
-                    <div class="flex items-center">
-                      <span
-                        v-if="client.ownerPhone != ''"
-                        class="mr-2 text-sm font-medium text-navy-700 dark:text-sky-300"
-                      >
-                        {{ client.ownerPhone }}
-                      </span>
-                      <span v-else class="mr-2 text-sm text-gray-300 dark:text-navy-700">
-                        600000000
-                      </span>
-                      <span v-if="client.owner != ''" class="capitalize text-navy-900 dark:text-sky-100">
-                        {{ client.owner }}
-                      </span>
-                    </div>
-                  </td>
-                  <!-- Comments -->
-                  <!-- <td class="py-3 px-2 text-left whitespace-nowrap">
-                    <div class="flex items-center text-navy-700 dark:text-sky-300">
-                      <small>{{ client.comment }}</small>
-                    </div>
-                  </td> -->
-                  <!-- Number of Dates -->
-                  <!-- <td class="py-3 px-2 text-left whitespace-nowrap">
-                    <div class="flex items-center justify-end">
-                      <span class="font-bold text-indigo-300">
-                        {{ client.dates.length }}
-                      </span>
-                    </div>
-                  </td> -->
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+                  Editar
+                </router-link>
+              </p>
+              <p class="mt-1">
+                <button
+                  @click="removeClient(client.id)"
+                  class="w-20 h-6 border-0 rounded py-1 px-3 bg-red-50 hover:bg-red-100 focus:outline-none text-red-500 hover:text-red-600 hover:underline text-xs tracking-wide"
+                >
+                  Eliminar
+                </button>
+              </p>
+            </td>
+
+          </tr>
+        </tbody>
+      </table>
     </div>
 
   </section>
@@ -147,6 +145,7 @@
 
 <script>
 import TheTitle from '@/components/TheTitle.vue'
+
 import { mapState, mapActions } from 'vuex'
 
 export default {
